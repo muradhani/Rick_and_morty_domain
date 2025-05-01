70184a21049276c419cef7293221bb77bd91c359
muradhani <mouradhany574@gmail.com>
2025-05-01 22:55:21 +0300

    refactor: Move data module to data folder and add hilt to data and network modules
    This commit refactors the project structure by moving the `DataModule` from the `app` module to the `data` module.
    Additionally, it integrates Hilt dependency injection into both the `data` and `network` modules.
    
    - Removed `DataModule.kt` from the `app` module.
    - Added `DataModule.kt` to the `data` module under `src/main/java/com/example/data/di`.
    - Added Hilt Android plugin and Kapt plugin to the `build.gradle.kts` of the `data` and `network` modules.
    - Added Hilt Android and Hilt Compiler dependencies to the `build.gradle.kts` of the `data` and `network` modules.
    - Removed `@Inject` annotation from the `GetCharacterUseCase` constructor in the `domain` module as it will be provided via a Hilt module.
    - Added `AppModule.kt` to the `app` module under `src/main/java/com/example/rickandmprty/di` to provide `GetCharacterUseCase` using Hilt.
    - Changed the constructor of `CharacterRepositoryImpl` to be `internal` and annotated it with `@Inject`.

--- src/main/java/com/example/domain/useCases/GetCharacterUseCase.kt
+++ src/main/java/com/example/domain/useCases/GetCharacterUseCase.kt
@@ -2,9 +2,8 @@ package com.example.domain.useCases
 
 import com.example.domain.interfaces.CharacterRepository
 import com.example.domain.modules.Character
-import javax.inject.Inject
 
-class GetCharacterUseCase @Inject constructor(
+class GetCharacterUseCase (
     private val repository: CharacterRepository
 ) {
     suspend operator fun invoke(id: Int): Character? {
