858d5ba0554940b994205717b89a08cff97423f6
muradhani <mouradhany574@gmail.com>
2025-05-17 23:28:58 +0300

    Add CharactersPage to CharacterRepository

--- src/main/java/com/example/domain/interfaces/CharacterRepository.kt
+++ src/main/java/com/example/domain/interfaces/CharacterRepository.kt
@@ -1,5 +1,8 @@
 package com.example.domain.interfaces
 
+import com.example.domain.modules.CharactersPage
+
 interface CharacterRepository {
     suspend fun getCharacter(id: Int): com.example.domain.modules.Character?
+    suspend fun getCharacters(pageNumber:Int): CharactersPage
 }
